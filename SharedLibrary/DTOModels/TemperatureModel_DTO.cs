2c01b5469c1f8f48631d867f907032bf6a3a154a
safaenet <safa.dh@gmail.com>
2022-11-16 18:55:41 +0330

    Add project files.

---
+++ SharedLibrary/DTOModels/TemperatureModel_DTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.DTOModels
+{
+    public class TemperatureModel_DTO
+    {
+        public int DeviceId { get; set; }
+        public double Celsius { get; set; }
+    }
+}
