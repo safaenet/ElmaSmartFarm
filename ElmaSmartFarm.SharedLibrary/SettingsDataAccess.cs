9239a2cebc6fa810dcf886789c0141691df7c4bf
Safa Seed <safa.dh@gmail.com>
2022-11-19 19:53:41 +0330

    0828

--- ElmaSmartFarm.SharedLibrary/SettingsDataAccess.cs
+++ ElmaSmartFarm.SharedLibrary/SettingsDataAccess.cs
@@ -9,14 +9,25 @@ namespace ElmaSmartFarm.SharedLibrary
 {
     public static class SettingsDataAccess
     {
+        const string relativePath = @"../ElmaSmartFarm.SharedLibrary";
         public static IConfiguration AppConfiguration()
         {
-            var relativePath = @"../Config";
             var absolutePath = Path.GetFullPath(relativePath);
             IConfiguration conf;
             var builder = new ConfigurationBuilder()
                 .SetBasePath(absolutePath)
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            conf = builder.Build();
+            return conf;
+        }
+
+        public static IConfiguration SystemConfiguration()
+        {
+            var absolutePath = Path.GetFullPath(relativePath);
+            IConfiguration conf;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(absolutePath)
+                .AddJsonFile("syssettings.json", optional: false, reloadOnChange: true);
             conf = builder.Build();
             return conf;
         }
