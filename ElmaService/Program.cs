f08ab1cd0d6cbd5ee736f7bd78db4ba5457d2512
safaenet <safa.dh@gmail.com>
2022-11-19 12:18:44 +0330

    0828

--- ElmaService/Program.cs
+++ ElmaService/Program.cs
@@ -1,18 +1,41 @@
 ﻿
 using MQTTnet;
 using MQTTnet.Client;
+using System.Text;
 
-public class Program
+namespace ElmaSmartFarm.Service
 {
-    public async Task Main(string[] args)
+    public class Program
     {
-        var mqttFactory = new MqttFactory();
-        IMqttClient mqttClient=mqttFactory.CreateMqttClient();
-        var options = new MqttClientOptionsBuilder()
-            .WithClientId(Guid.NewGuid().ToString())
-            .WithTcpServer("192.168.1.106", 1883)
-            .WithCleanSession()
-            .Build();
-        await mqttClient.ConnectAsync(options);
+        public static async Task Main(string[] args)
+        {
+            string mqtt_broker = SettingsDataAccess.AppConfiguration().GetSection("mqtt:mqtt_broker").Value ?? "192.168.1.106";
+            int mqtt_port = int.Parse(SettingsDataAccess.AppConfiguration().GetSection("mqtt:mqtt_port").Value ?? "1883");
+            string mqtt_username = SettingsDataAccess.AppConfiguration().GetSection("mqtt:mqtt_username").Value ?? "admin";
+            string mqtt_password = SettingsDataAccess.AppConfiguration().GetSection("mqtt:mqtt_password").Value ?? "admin";
+            MqttFactory mqttFactory = new();
+            IMqttClient mqttClient = mqttFactory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                .WithClientId(Guid.NewGuid().ToString())
+                .WithTcpServer(mqtt_broker, mqtt_port)
+                .WithCleanSession()
+                .Build();
+            await mqttClient.ConnectAsync(options);
+
+            var mqttTopicFilterBuilder = new MqttTopicFilterBuilder().WithTopic("safa/#").Build();
+            await mqttClient.SubscribeAsync(mqttTopicFilterBuilder);
+            mqttClient.ApplicationMessageReceivedAsync += MqttClient_ApplicationMessageReceivedAsync;
+            Console.WriteLine("Connected to MQTT");
+            while (true) ;
+            //var m = new MqttApplicationMessageBuilder().WithTopic("safa").WithPayload("dana").Build();
+            //if(mqttClient.IsConnected) await mqttClient.PublishAsync(m);
+        }
+
+        private static Task MqttClient_ApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs arg)
+        {
+            Console.Write(arg.ApplicationMessage.Topic + ": ");
+            Console.WriteLine(Encoding.UTF8.GetString(arg.ApplicationMessage.Payload));
+            return Task.CompletedTask;
+        }
     }
 }
---
+++ ElmaService/SettingsDataAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElmaSmartFarm.Service
+{
+    public static class SettingsDataAccess
+    {
+        public static IConfiguration AppConfiguration()
+        {
+            IConfiguration conf;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            conf = builder.Build();
+            return conf;
+        }
+    }
+}
