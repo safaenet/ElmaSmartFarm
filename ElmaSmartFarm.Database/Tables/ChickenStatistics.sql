9bf407da0d742194dcfd5d1df7c3154b85a0f400
safaenet <safa.dh@gmail.com>
2022-11-22 18:19:20 +0330

    0901

--- ElmaSmartFarm.Database/Tables/ChickenStatistics.sql
+++ ElmaSmartFarm.Database/Tables/ChickenStatistics.sql
@@ -2,6 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [PeriodId] INT NOT NULL, 
+    [FarmId] INT NOT NULL, 
     [DeadCount] INT NOT NULL, 
     [DateHappened] DATETIME NOT NULL, 
     [DateRegistered] DATETIME NOT NULL
--- ElmaSmartFarm.Database/Tables/FoodStatistics.sql
+++ ElmaSmartFarm.Database/Tables/FoodStatistics.sql
@@ -2,6 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [PeriodId] INT NOT NULL, 
+    [FarmId] INT NOT NULL, 
     [FoodWeight] INT NOT NULL, 
     [FeedDate] DATETIME NOT NULL, 
     [DateRegistered] DATETIME NOT NULL
