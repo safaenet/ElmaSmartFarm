39749607bb05c9cda59b5b682d077183fe1ae98a
Safa Seed <safa.dh@gmail.com>
2022-12-18 00:52:08 +0330

    0926

--- ElmaSmartFarm.Database/Tables/AlarmDevices.sql
+++ ElmaSmartFarm.Database/Tables/AlarmDevices.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [dbo].[AlarmDevices]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [Type] TINYINT NOT NULL, 
     [Name] NVARCHAR(200) NULL,
+    [Type] TINYINT NOT NULL, 
+	[LocationId] INT NOT NULL, 
     [IsEnabled] BIT NOT NULL DEFAULT 1, 
     [Descriptions] NVARCHAR(200) NULL
 )
--- ElmaSmartFarm.SharedLibrary/Models/Alarm/AlarmBaseModel.cs
+++ ElmaSmartFarm.SharedLibrary/Models/Alarm/AlarmBaseModel.cs
@@ -5,6 +5,9 @@ public class AlarmBaseModel
     public int Id { get; set; }
     public string Name { get; set; }
     public AlarmDeviceType Type { get; set; }
+    /// <summary>
+    /// The location (Farm/Poultry) to which this alarm device is attached to. This is needed for mqtt message.
+    /// </summary>
     public int LocationId { get; set; }
     public bool IsEnabled { get; set; }
     public string Descriptions { get; set; }
