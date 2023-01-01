a729dac02fa2cc7856f1a6e04d7f19a72047f19a
safaenet <safa.dh@gmail.com>
2023-01-01 17:06:10 +0330

    0

---
+++ ElmaSmartFarm.ClientWpf/Bootstrapper.cs
@@ -0,0 +1,67 @@
+﻿using Caliburn.Micro;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace ElmaSmartFarm.ClientWpf;
+
+public class Bootstrapper : BootstrapperBase
+{
+    public SimpleContainer Container = new();
+    protected override object GetInstance(Type serviceType, string key)
+    {
+        return Container.GetInstance(serviceType, key);
+    }
+
+    protected override void Configure()
+    {
+        Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.File(@"log\LogFile.txt")
+                .CreateLogger();
+
+        try
+        {
+            Container.Instance(Container)
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(ViewModelType => Container.RegisterPerRequest(
+                    ViewModelType, ViewModelType.ToString(), ViewModelType));
+
+            //Container
+            //    .Singleton<IApiProcessor, ApiProcessor>();
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Error in Bootstrapper");
+        }
+    }
+
+    protected override IEnumerable<object> GetAllInstances(Type serviceType)
+    {
+        return Container.GetAllInstances(serviceType);
+    }
+
+    protected override void BuildUp(object instance)
+    {
+        Container.BuildUp(instance);
+    }
+    public Bootstrapper()
+    {
+        Initialize();
+    }
+
+    protected override void OnStartup(object sender, StartupEventArgs e)
+    {
+        //_ = DisplayRootViewFor<LoginViewModel>();
+    }
+}
--- ElmaSmartFarm.WebApi/Program.cs
+++ ElmaSmartFarm.WebApi/Program.cs
@@ -1,25 +1,72 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
+using Serilog.Events;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Debug()
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                    .Enrich.FromLogContext()
+                    .WriteTo.File(@"log\LogFile.txt")
+                    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+        {
+            Description = "Standard authorization",
+            In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+            Name = "Authorization",
+            Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey
+        });
+        options.OperationFilter<SecurityRequirementsOperationFilter>();
+    });
+
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateLifetime = true,
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            //ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            //ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            //ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+    //builder.Services
+    //                .AddScoped<IUserProcessor, SqlUserProcessor>()
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Error(ex, "Error in Program.cs");
+}
