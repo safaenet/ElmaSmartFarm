9203c1fee1fbccdcbdcbe2c4f6b43e31c7762dd3
Safa Seed <safa.dh@gmail.com>
2023-01-16 00:12:02 +0330

    0

---
+++ ElmaSmartFarm.FarmViewerUserControl/AllInOneSensorViewer.xaml.cs
@@ -0,0 +1,27 @@
+﻿using ElmaSmartFarm.SharedLibrary.Models.Sensors;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ElmaSmartFarm.UserControls;
+/// <summary>
+/// Interaction logic for AllInOneSensorViewer.xaml
+/// </summary>
+public partial class AllInOneSensorViewer : UserControl
+{
+    public AllInOneSensorViewer()
+    {
+        InitializeComponent();
+    }
+
+    public ScalarSensorModel ScalarSensor
+    {
+        get { return (ScalarSensorModel)GetValue(ScalarSensorProperty); }
+        set { SetValue(ScalarSensorProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty ScalarSensorProperty =
+        DependencyProperty.Register("ScalarSensor", typeof(ScalarSensorModel), typeof(AllInOneSensorViewer), new PropertyMetadata(null));
+
+
+}
--- ElmaSmartFarm.FarmViewerUserControl/FarmViewer.xaml.cs
+++ ElmaSmartFarm.FarmViewerUserControl/FarmViewer.xaml.cs
@@ -1,17 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ElmaSmartFarm.SharedLibrary.Models;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ElmaSmartFarm.UserControls
 {
@@ -24,5 +13,17 @@ namespace ElmaSmartFarm.UserControls
         {
             InitializeComponent();
         }
+
+        public FarmModel Farm
+        {
+            get { return (FarmModel)GetValue(FarmProperty); }
+            set { SetValue(FarmProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Farm.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FarmProperty =
+            DependencyProperty.Register("Farm", typeof(FarmModel), typeof(FarmViewer), new PropertyMetadata(null));
+
+
     }
 }
