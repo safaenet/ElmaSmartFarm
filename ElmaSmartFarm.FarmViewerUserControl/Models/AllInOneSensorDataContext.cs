eabd7c4209925b7df6f57c8c91921cf6e31c9d6d
Safa Seed <safa.dh@gmail.com>
2023-01-16 21:18:51 +0330

    0

---
+++ ElmaSmartFarm.FarmViewerUserControl/Models/AllInOneSensorDataContext.cs
@@ -0,0 +1,21 @@
+﻿using ElmaSmartFarm.SharedLibrary.Models.Sensors;
+using System.ComponentModel;
+
+namespace ElmaSmartFarm.UserControls.Models;
+
+public class AllInOneSensorDataContext : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private ScalarSensorModel scalar;
+
+    public ScalarSensorModel Scalar
+    {
+        get { return scalar; }
+        set { scalar = value; OnPropertyChanged(nameof(Scalar)); }
+    }
+    protected void OnPropertyChanged(string info)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+    }
+}
